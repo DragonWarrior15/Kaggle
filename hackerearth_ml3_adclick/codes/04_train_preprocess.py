d06609d5c44773005df9c1b827103e543c69b1b4
Vaibhav Ojha <vaibhav.ojha60@gmail.com>
2017-08-01 22:10:17 +0530

    added code for creating feature based on click rate of merchants, catrgories etc

--- hackerearth_ml3_adclick/codes/04_train_preprocess.py
+++ hackerearth_ml3_adclick/codes/04_train_preprocess.py
@@ -15,9 +15,9 @@ from sklearn.ensemble import RandomForestClassifier
 np.random.seed(42)
 
 # df = pd.read_csv(c_vars.train_file)
-# df = pd.read_csv(c_vars.train_split_train_sample, nrows = 100000)
+# df = pd.read_csv(c_vars.train_split_train_sample, nrows = 1000)
 df = pd.read_csv(c_vars.train_split_train)
-df = df[c_vars.header_useful]
+# df = df[c_vars.header_useful]
 
 df.fillna(c_vars.fillna_dict, inplace = True)
 
@@ -30,11 +30,54 @@ df.loc[:, 'datetime_hour'] = df['datetime'].apply(lambda x: datetime.strptime(x,
 df = df.drop('datetime', axis=1)
 df.loc[:, 'browserid'] = df['browserid'].apply(lambda x: x if x not in c_vars.browserid_map 
                                                            else c_vars.browserid_map[x])
-print(df.columns.values)
-c_vars.header_useful.remove('datetime')
-c_vars.header_useful.remove('click')
+
+# merchant, siteid, offerid, category
+df_feature = {}
+for col in ['merchant', 'siteid', 'offerid', 'category']:
+# for col in ['merchant']:
+    df_temp = df[[col, 'click']]
+    df_temp = df_temp.groupby([col]).agg(['count', np.sum])
+    df_temp.reset_index(inplace = True)
+    df_temp['count'] = df_temp['click', 'count']
+    df_temp['num_1'] = df_temp['click', 'sum']
+    df_temp['num_0'] = df_temp['count'] - df_temp['num_1']
+    df_temp = df_temp[[col, 'count', 'num_0', 'num_1']]
+    df_temp.columns = df_temp.columns.get_level_values(0)
+    df_temp.sort_values('count', inplace = True, axis = 0, ascending = False)
+    df_temp['cumul_sum'] = np.cumsum(df_temp['count'])
+    df_temp_2 = df_temp.loc[df_temp['cumul_sum'] > 0.8 * len(df), :]
+    df_temp = df_temp.loc[~(df_temp['cumul_sum'] > 0.8 * len(df)), :]
+    df_temp = df_temp.append({col:'LESS_FREQ', 
+                                  'count':np.sum(df_temp_2['count']), 
+                                  'num_0':np.sum(df_temp_2['num_0']),
+                                  'num_1':np.sum(df_temp_2['num_1'])},
+                             ignore_index = True)
+    df_temp['click_rate'] = df_temp['num_1']/df_temp['count']
+    df_temp.drop(['cumul_sum'], inplace = True, axis = 1)
+    df_feature[col] = df_temp.loc[:,:]
+
+    df = pd.merge(df, df_temp, how = 'left', on = col, suffixes = ('', ''))
+    df.rename(columns = {'count':'count_'+col, 'num_0':'num_0_'+col, 
+                         'num_1':'num_1_'+col, 'click_rate':'click_rate_'+col}, 
+              inplace = True)
+    for field in ['count', 'num_0', 'num_1', 'click_rate']:
+        df[field + '_' + col].fillna(df_feature[col].loc[df_temp[col] == 'LESS_FREQ', field].values[0], inplace = True)
+    # print (df.columns.values)
+
+    # print (df)
+# print (df)
+print (df.columns.values)
+
+for col in ['datetime', 'click', 'merchant', 'siteid', 'offerid', 'category']:
+    c_vars.header_useful.remove(col)
+
 c_vars.header_useful.append('datetime_day')
 c_vars.header_useful.append('datetime_hour')
+
+for col in ['merchant', 'siteid', 'offerid', 'category']:
+    for col2 in ['count', 'num_0', 'num_1', 'click_rate']:
+        c_vars.header_useful.append(col2 + '_' + col)
+
 print (c_vars.header_useful)
 for col in ['countrycode', 'browserid', 'devid', 'datetime_day', 'datetime_hour']:
     print (df[col].unique().tolist())
@@ -43,7 +86,7 @@ X = df[c_vars.header_useful].as_matrix()
 y = df['click'].as_matrix()
 
 print (str(datetime.now()) + ' Label Encoding Started')
-label_encoder = [LabelEncoder() for _ in range(len(c_vars.header_useful))]
+label_encoder = [LabelEncoder() for _ in range(5)]
 for i in range(len(label_encoder)):
     label_encoder[i].fit(X[:,i])
     # print (i, c_vars.header_useful[i], label_encoder[i].get_params(deep=True))
@@ -53,14 +96,14 @@ print (str(datetime.now()) + ' Label Encoding Completed')
 print (str(datetime.now()) + ' One Hot Encoding Started')
 cols_to_ohe = ['datetime', 'countrycode', 'browserid', 'devid']
 ohe = OneHotEncoder(sparse = False)
-ohe.fit(X[:,[4,5,6,7,8]])
+ohe.fit(X[:,[0,1,2,3,4]])
 # print (ohe.get_params(deep=True))
-X_ohe = ohe.transform(X[:,[4,5,6,7,8]])
+X_ohe = ohe.transform(X[:,[0,1,2,3,4]])
 print (str(datetime.now()) + ' One Hot Encoding Complete')
 print (X.shape, X_ohe.shape)
 # print (X.shape, X_ohe.shape)
 # print (X, X_ohe)
-X = np.hstack((X[:,[i for i in range(len(c_vars.header_useful)) if i not in [4,5,6,7,8]]], X_ohe))
+X = np.hstack((X[:,[i for i in range(len(c_vars.header_useful)) if i not in [0,1,2,3,4]]], X_ohe))
 
 print (X.shape, y.shape, np.sum(y))
 sm = SMOTE(random_state=42)
@@ -78,3 +121,4 @@ clf = RandomForestClassifier(n_estimators = 200, max_depth = 10, min_samples_lea
 clf.fit(X, y)
 print (clf.feature_importances_)
 # feature_importances_
+
