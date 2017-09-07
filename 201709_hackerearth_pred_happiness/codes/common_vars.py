babe0c96d532065ffde206610a9b88ac39196fd8
DragonWarrior15 <vaibhav.ojha60@gmail.com>
2017-09-07 23:53:40 +0530

    updated common_vars.py

--- 201709_hackerearth_pred_happiness/codes/common_vars.py
+++ 201709_hackerearth_pred_happiness/codes/common_vars.py
@@ -33,3 +33,11 @@ target_dict = {
     'happy' : 1,
     'not_happy' : 0
 }
+
+def add_feature(X, feature_to_add):
+    '''
+    Returns sparse feature matrix with added feature.
+    feature_to_add can also be a list of features.
+    '''
+    from scipy.sparse import csr_matrix, hstack
+    return hstack([X, csr_matrix(feature_to_add).T], 'csr')
