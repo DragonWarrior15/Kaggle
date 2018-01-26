786c5176d35e674d01e3cceb1518bcbe0d39a783
DragonWarrior15 <vaibhav.ojha60@gmail.com>
2018-01-27 02:24:44 +0530

    analyticsvidhya dota2 | updated cf code to use tensorflow and adam optimizer

--- 201801_analyticsvidhya_dota2/codes/11_collaborative_regression.py
+++ 201801_analyticsvidhya_dota2/codes/11_collaborative_regression.py
@@ -3,10 +3,18 @@ import numpy as np
 import os
 import sys
 import pickle
+import tensorflow as tf
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 epsilon = 1e-07
-num_iterations = 100
-learning_rate = 0.01
+num_iterations = 3000
+learning_rate = 0.001
+reg_alpha = 0.5
 
 with open('../inputs/hero_id_dict', 'rb') as f:
     hero_id_dict = pickle.load(f)
@@ -48,7 +56,7 @@ num_heroes = len(df_heroes['hero_id'].unique())
 df_heroes['hero_id'] = 1
 hero_matrix = df_heroes.as_matrix()
 # standard scale the hero_matrix
-hero_matrix = (hero_matrix - np.mean(hero_matrix, axis = 0))/(np.std(hero_matrix, axis = 0) + epsilon)
+# hero_matrix = (hero_matrix - np.mean(hero_matrix, axis = 0))/(np.std(hero_matrix, axis = 0) + epsilon)
 hero_matrix[:, 0] = 1
 # print (hero_matrix)
 
@@ -62,21 +70,17 @@ user_hero_num_games = df_train_full.pivot_table(index = 'user_id', columns = 'he
 
 user_hero_exist = np.clip(np.array(user_hero_num_games), a_min = 0, a_max = 1)
 
+hero_matrix   = tf.constant(hero_matrix  , dtype = tf.float32)
+user_hero_kda = tf.constant(user_hero_kda, dtype = tf.float32)
 
 # start the cross validation loop, use 10 cross validations
 seed_list = [100 * (x + 1) + 10 * (x + 2) + (x + 3) for x in range(10)]
-train_history = {'train_loss':[], 'val_loss':[]}
-for cross_val in range(1):
+for cross_val in range(5):
+    train_history = {'train_loss':[], 'val_loss':[]}
     # define two numpy arrays, one stores 1 at the combinations of 
     # train user X hero, and other stores for val
-    is_val = np.zeros((num_users, num_heroes))
+    is_val   = np.zeros((num_users, num_heroes))
     is_train = np.array(user_hero_exist)
-
-    # define the attribute matrix, num of columns is same as no of 
-    # hero attributes, add a bias term also
-    # user_matrix = np.random.randn(num_users, num_hero_attr)
-    user_matrix = np.zeros((num_users, num_hero_attr))
-
     # split the data into train and validation sets
     # validation set will comprise of a single rating removed from each
     # of the users
@@ -87,42 +91,55 @@ for cross_val in range(1):
         col = df_val.iloc[i, train_cols_dict['hero_id']]
         is_val[row][col] = 1
         is_train[row][col] = 0
-
     ## scale and transform the num_games values
     # get all the list of num_games
     is_train_num_games_list = user_hero_num_games[is_train == 1]
     num_games_mean = np.mean(is_train_num_games_list)
-    num_games_std  = np.std(is_train_num_games_list)
+    num_games_std  = np.std(is_train_num_games_list) + epsilon
+    
     # standard scale those values
-    user_hero_num_games_train = (user_hero_num_games - num_games_mean)/num_games_std
+    user_hero_num_games_train = tf.constant((user_hero_num_games - num_games_mean)/num_games_std, dtype = tf.float32)
 
-    ## begin the training loop
+    is_val   = tf.constant(is_val  , dtype = tf.float32)
+    is_train = tf.constant(is_train, dtype = tf.float32)
+
+
+    # define the attribute matrix, num of columns is same as no of 
+    # hero attributes, add a bias term also
+    user_matrix           = tf.Variable(tf.zeros([num_users, num_hero_attr]), dtype = tf.float32)
+    user_matrix_num_games = tf.Variable(tf.zeros([num_users, 1])            , dtype = tf.float32)
+
+    kda_pred      = tf.matmul(user_matrix, tf.transpose(hero_matrix)) + tf.multiply(user_matrix_num_games, user_hero_num_games_train)
+    # kda_pred      = tf.matmul(user_matrix, tf.transpose(hero_matrix))
+    error         = kda_pred - user_hero_kda
+    cost          = tf.reduce_sum(tf.square(tf.multiply(is_train, error)))/tf.reduce_sum(is_train)
+    cost_training = cost + reg_alpha * (tf.reduce_sum(tf.square(user_matrix) + tf.square(user_matrix_num_games)))/tf.reduce_sum(is_train)
+    cost_train    = tf.sqrt(cost)
+    cost_val      = tf.sqrt(tf.reduce_sum(tf.square(tf.multiply(is_val, error)))/tf.reduce_sum(is_val))
+    
+    # train_step = tf.train.AdagradOptimizer(learning_rate).minimize(cost_training)
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(cost_training)
+    # train_step  = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_training)
+    
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+
     for iteration in range(num_iterations):
-        # calculate the cost
-        cost = np.matmul(user_matrix, hero_matrix.T) + (user_hero_num_games_train)
-        cost_cache = np.multiply(is_train, cost) - np.multiply(is_train, user_hero_kda)
-        cost = np.power(np.sum(cost), 2)/(2 * np.sum(is_train))
-        # update the train history
-        train_history['train_loss'].append(cost)
+        sess.run(train_step)
+        train_history['train_loss'].append(sess.run(cost_train))
+        train_history['val_loss'].append(sess.run(cost_val))
 
-        ## update the weights
-        # loop over all the weights in the matrix
-        user_matrix -= (learning_rate * np.matmul(np.multiply(is_train, cost_cache), hero_matrix))/np.sum(is_train)
+        if (iteration + 1)% 500 == 0:
+            print (cross_val, iteration, train_history['train_loss'][-1], train_history['val_loss'][-1])
 
-        ## little more detailed logic using for loos
-        # for row in range(user_matrix.shape[0]):
-            # for col in range(user_matrix.shape[1]):
-                # gradient = 0
-                # loop over all the heros for which cost was calculated
-                # gradient += cost_cache[row][col] * np.multiply(is_train[row, :], hero_matrix.T[col, :])
-                # update the weights
-                # user_matrix[row][col] -= (learning_rate * gradient)/np.sum(is_train)
-
-        # calculate the validation loss
-        cost = np.matmul(user_matrix, hero_matrix.T) + (user_hero_num_games_train)
-        cost_cache = np.multiply(is_val, cost) - np.multiply(is_val, user_hero_kda)
-        cost = np.power(np.sum(cost), 2)/(2 * np.sum(is_val))
-        # update the train_history
-        train_history['val_loss'].append(cost)
-
-        print (cross_val, iteration, train_history['train_loss'][-1], train_history['val_loss'][-1])
+    with sns.plotting_context(font_scale=1.3):
+        fig, axs = plt.subplots(nrows = 1, figsize=(18, 8))
+        sns.set(font_scale=1.5)
+        sns.set_style("darkgrid")
+        plt.plot(train_history['train_loss'], label = 'Train'     , color = 'b', linewidth = 1)
+        plt.plot(train_history['val_loss']  , label = 'Validation', color = 'r', linewidth = 2)
+        legend = axs.legend(loc='upper right')
+        plt.xlabel('Epoch')
+        plt.ylabel('RMSE')
+        fig.savefig('../graphs/collaborative_filtering_tf_v1_cross_val_' + str(cross_val) + '.png', dpi = 300)
